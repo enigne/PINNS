c9cb99f6ac96f1b69dcfdbc256cc09914ff7b73b
Cheng Gong <enigne@gmail.com>
2023-02-08 14:29:05 -0500

    add H, bed, C to the output of NN with u and v

--- SSA_equations/SSANN.py
+++ SSA_equations/SSANN.py
@@ -243,6 +243,169 @@ class SSAInformedNN(NeuralNetwork): #{{{
         h_pred = self.model(X_star)
         return  tf.math.reduce_euclidean_norm(h_pred - u_star[:,0:2]) / tf.math.reduce_euclidean_norm(u_star[:,0:2])
     #}}}
+class SSAAllNN(NeuralNetwork): #{{{
+    def __init__(self, hp, logger, X_f, 
+            X_bc, u_bc, X_cf, n_cf, 
+            xub, xlb, uub, ulb, 
+            modelPath, reloadModel,
+            eta, n=3.0, 
+            loss_weights=[1e-2, 1e-6, 1e-10],
+            geoDataNN=None, FrictionCNN=None):
+        super().__init__(hp, logger, xub, xlb, uub, ulb, modelPath, reloadModel=reloadModel)
+
+        # weights of the loss functions
+        self.loss_weights = tf.constant(loss_weights, dtype=self.dtype)
+            
+        # scaling factors
+        self.ub = tf.constant(xub, dtype=self.dtype)
+        self.lb = tf.constant(xlb, dtype=self.dtype)
+
+        # Dirichlet B.C.
+        self.X_bc = self.tensor(X_bc)
+        self.u_bc = self.tensor(u_bc)
+
+        # Calving front
+        self.X_cf = self.tensor(X_cf)
+        self.n_cf = self.tensor(n_cf)
+
+        # viscosity
+        self.eta = tf.constant(eta, dtype=self.dtype)
+        self.n = tf.constant(n, dtype=self.dtype)
+
+        # some constants
+        self.rhoi = tf.constant(917, dtype=self.dtype)  # kg/m^3
+        self.rhow = tf.constant(1023, dtype=self.dtype) # kg/m^3
+        self.g = tf.constant(9.81, dtype=self.dtype)    # m/s^2
+        self.yts = tf.constant(3600.0*24*365, dtype=self.dtype)
+
+        # Separating the collocation coordinates
+        self.x_f = self.tensor(X_f[:, 0:1])
+        self.y_f = self.tensor(X_f[:, 1:2])
+
+    # get the velocity and derivative information
+    @tf.function
+    def uvHbC_model(self, X):
+        x = X[:, 0:1]
+        y = X[:, 1:2]
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(x)
+            tape.watch(y)
+            Xtemp = tf.concat([x, y], axis=1)
+
+            sol = self.model(Xtemp)
+            u = sol[:, 0:1]
+            v = sol[:, 1:2]
+            H = sol[:, 2:3]
+            bed = sol[:, 3:4]
+            C = sol[:, 4:5]
+
+        u_x = tape.gradient(u, x)
+        v_x = tape.gradient(v, x)
+        u_y = tape.gradient(u, y)
+        v_y = tape.gradient(v, y)
+        del tape
+
+        return u, v, u_x, v_x, u_y, v_y, H, bed, C
+
+    # The actual PINN
+    @tf.function
+    def f_model(self):
+        # viscosity
+        eta = self.eta
+        n = self.n
+
+        # Using the new GradientTape paradigm of TF2.0,
+        # which keeps track of operations to get the gradient at runtime
+        with tf.GradientTape(persistent=True) as tape:
+            # Watching the two inputs we’ll need later, x and y
+            tape.watch(self.x_f)
+            tape.watch(self.y_f)
+            # Packing together the inputs
+            X_f = tf.concat([self.x_f, self.y_f], axis=1)
+
+            # Getting the prediction
+            u, v, u_x, v_x, u_y, v_y H, bed, C = self.uvx_model(X_f)
+            h = H + bed
+
+            epsilon = 0.5*eta *(u_x**2 + v_y**2 + 0.25*(u_y+v_x)**2 + u_x*v_y+1.0e-30)**(0.5*(1.0-n)/n)
+            # stress tensor
+            etaH = epsilon * H
+            B11 = etaH*(4*u_x + 2*v_y)
+            B22 = etaH*(4*v_y + 2*u_x)
+            B12 = etaH*(  u_y +   v_x)
+
+        # Getting the other derivatives
+        sigma11 = tape.gradient(B11, self.x_f)
+        sigma12 = tape.gradient(B12, self.y_f)
+
+        sigma21 = tape.gradient(B12, self.x_f)
+        sigma22 = tape.gradient(B22, self.y_f)
+                
+        # surface gradient
+        h_x = tape.gradient(h, self.x_f)
+        h_y = tape.gradient(h, self.y_f)
+
+        # Letting the tape go
+        del tape
+        u_norm = (u**2+v**2)**0.5
+        alpha = C**2 * (u_norm)**(1.0/self.n)
+
+        f1 = sigma11 + sigma12 - alpha*u/(u_norm+1e-30) - self.rhoi*self.g*H*h_x
+        f2 = sigma21 + sigma22 - alpha*v/(u_norm+1e-30) - self.rhoi*self.g*H*h_y
+
+        return f1, f2
+
+    @tf.function
+    def loss(self, uv, uv_pred):
+        # Dirichlet B.C. for u and v
+        ubc = self.u_bc[:, 0:1]
+        vbc = self.u_bc[:, 1:2]
+        sol_bc_pred = self.model(self.X_bc)
+        ubc_pred = sol_bc_pred[:,0:1]
+        vbc_pred = sol_bc_pred[:,1:2]
+        
+        # match H, bed, and C to the training data
+        H0 = uv[:,2:3]
+        bed0 = uv[:,3:4]
+        C0 = uv[:,4:5]
+        H0_pred = uv_pred[:,2:3]
+        bed0_pred = uv_pred[:,3:4]
+        C0_pred = uv_pred[:,4:5]
+
+        # f_model on the collocation points 
+        f1_pred, f2_pred = self.f_model()
+
+        # Calving on X_cf
+       # fc1_pred, fc2_pred = self.cf_model(self.X_cf, self.n_cf)
+
+        # misfits
+        mse_u = self.loss_weights[0]*(self.yts**2) * tf.reduce_mean(tf.square(ubc - ubc_pred))
+        mse_v = self.loss_weights[0]*(self.yts**2) * tf.reduce_mean(tf.square(vbc - vbc_pred))
+
+        mse_H = self.loss_weights[1]*tf.reduce_mean(tf.square(H0 - H0_pred))
+        mse_bed = self.loss_weights[1]*tf.reduce_mean(tf.square(bed0 - bed0_pred))
+        mse_C = self.loss_weights[2]* tf.reduce_mean(tf.square(C0 - C0_pred))
+
+        mse_f1 = self.loss_weights[3]*tf.reduce_mean(tf.square(f1_pred))
+        mse_f2 = self.loss_weights[3]*tf.reduce_mean(tf.square(f2_pred))
+#        mse_fc1 = self.loss_weights[2]*tf.reduce_mean(tf.square(fc1_pred))
+#        mse_fc2 = self.loss_weights[2]*tf.reduce_mean(tf.square(fc2_pred))
+#
+        return mse_u + mse_v + \
+                mse_f1 + mse_f2 + \
+                mse_H + mse_bed + mse_C
+
+    def predict(self, X_star):
+        h_pred = self.model(X_star)
+        u_pred = h_pred[:, 0:1]
+        v_pred = h_pred[:, 1:2]
+        return u_pred.numpy(), v_pred.numpy()
+
+    @tf.function
+    def test_error(self, X_star, u_star):
+        h_pred = self.model(X_star)
+        return  tf.math.reduce_euclidean_norm(h_pred - u_star[:,0:2]) / tf.math.reduce_euclidean_norm(u_star[:,0:2])
+    #}}}
     
     
 class HBedDNN(NeuralNetwork): #{{{
@@ -299,7 +462,6 @@ class HBedDNN(NeuralNetwork): #{{{
         h_pred = self.model(X_star)
         return  tf.math.reduce_euclidean_norm(h_pred - u_star[:,0:2]) / tf.math.reduce_euclidean_norm(u_star[:,0:2])
     #}}}
-    
 class FrictionCDNN(NeuralNetwork): #{{{
     def __init__(self, hp, logger, X_f, xub, xlb, uub, ulb, modelPath, reloadModel=False):
         super().__init__(hp, logger, xub, xlb, uub, ulb, modelPath, reloadModel)
