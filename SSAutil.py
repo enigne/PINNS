76c3194284bbdab5e892eb8a277ba9155bf8fd4f
Cheng Gong <enigne@gmail.com>
2023-02-13 11:21:07 -0500

    set Dirichlet B.C. at the CF for Helheim

--- SSAutil.py
+++ SSAutil.py
@@ -273,3 +273,62 @@ def prep_Helheim_data_all(path, N_u=None, N_f=None): #{{{
     n_cf = np.hstack((nx.flatten()[:,None], ny.flatten()[:,None]))
 
     return x, y, Exact_vx, Exact_vy, X_star, u_star, X_u_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb  #}}}
+def prep_Helheim_Dirichlet(path, N_u=None, N_f=None): #{{{
+    # Reading SSA ref solutions: x, y-coordinates, usol and Hsol
+    data = scipy.io.loadmat(path,  mat_dtype=True)
+
+    # Flatten makes [[]] into [], [:,None] makes it a column vector
+    x = data['x'].flatten()[:,None]
+    y = data['y'].flatten()[:,None]
+
+    # collocation points
+    X_f = np.real(data['X_f'])
+    idf = np.random.choice(X_f.shape[0], N_f, replace=False)
+    X_f = X_f[idf,:]
+
+    # real() is to make it float by default, in case of zeroes
+    Exact_vx = np.real(data['vx'].flatten()[:,None])
+    Exact_vy = np.real(data['vy'].flatten()[:,None])
+    Exact_C = np.real(data['C'].flatten()[:,None])
+    Exact_H = np.real(data['H'].flatten()[:,None])
+    Exact_b = np.real(data['b'].flatten()[:,None])
+
+    # Preparing the inputs x and y for predictions in one single array, as X_star
+    X_star = np.hstack((x.flatten()[:,None], y.flatten()[:,None]))
+
+    # Preparing the testing u_star and vy_star
+    u_star = np.hstack((Exact_vx.flatten()[:,None], Exact_vy.flatten()[:,None], Exact_H.flatten()[:,None], Exact_b.flatten()[:,None], Exact_C.flatten()[:,None] )) 
+
+    # Domain bounds: for regularization and generate training set
+    xlb = X_star.min(axis=0)
+    xub = X_star.max(axis=0) 
+    ulb = u_star.min(axis=0)
+    uub = u_star.max(axis=0) 
+
+    # load Dirichlet boundary conditions
+    X_bc = np.real(data['X_bc'])
+    u_bc = np.real(data['u_bc'])
+
+    # Stacking them in multidimensional tensors for training, only use ice covered area
+    icemask = data['icemask'].flatten()[:,None]
+    iice = np.transpose(np.asarray(icemask>0).nonzero())
+    X_u_train = np.vstack([X_star[iice[:,0],:]])
+    u_train = np.vstack([u_star[iice[:,0],:]])
+
+    # Generating a uniform random sample from ints between 0, and the size of x_u_train, of size N_u (initial data size) and without replacement (unique)
+    idx = np.random.choice(X_u_train.shape[0], N_u, replace=False)
+    # Getting the corresponding X_u_train (which is now scarce boundary/initial coordinates)
+    X_u_train = X_u_train[idx,:]
+    # Getting the corresponding u_train
+    u_train = u_train [idx,:]
+
+    # calving front info
+    cx = data['cx'].flatten()[:,None]
+    cy = data['cy'].flatten()[:,None]
+    nx = data['smoothnx'].flatten()[:,None]
+    ny = data['smoothny'].flatten()[:,None]
+
+    X_cf = np.hstack((cx.flatten()[:,None], cy.flatten()[:,None]))
+    n_cf = np.hstack((nx.flatten()[:,None], ny.flatten()[:,None]))
+
+    return x, y, Exact_vx, Exact_vy, X_star, u_star, X_u_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb  #}}}
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ sys.path.append("./utils")
 from custom_lbfgs import *
 from SSAutil import *
 from neuralnetwork import NeuralNetwork, MinmaxScaleLayer, UpScaleLayer
-from SSA_equations.SSANN import SSANN_calvingfront
+from SSA_equations.SSANN import SSANN_invertC
 from logger import Logger
 from plotting import *
 import matplotlib.pyplot as plt
@@ -44,22 +44,24 @@ repoPath = "./"
 appDataPath = os.path.join(repoPath, "matlab_SSA", "DATA")
 #path = os.path.join(appDataPath, "SSA2D_nocalving.mat")
 #path = os.path.join(appDataPath, "SSA2D_seg_nocalving.mat")
-path = os.path.join(appDataPath, "SSA2D_segCF.mat")
+#path = os.path.join(appDataPath, "SSA2D_segCF.mat")
+path = os.path.join(appDataPath, "Helheim_Weertman_iT080_PINN_Dirichlet.mat")
+
 modelPath = "./Models/test"
 reloadModel = False # reload from previous training
 #}}}
 
 # load the data
-x, y, Exact_vx, Exact_vy, X_star, u_star, X_u_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb = prep_Helheim_data_all(path, hp["N_u"], hp["N_f"])
+x, y, Exact_vx, Exact_vy, X_star, u_star, X_u_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb = prep_Helheim_Dirichlet(path, hp["N_u"], hp["N_f"])
 
 # Creating the model and training
 logger = Logger(hp)
-pinn = SSANN_calvingfront(hp, logger, X_f,
+pinn = SSANN_invertC(hp, logger, X_f,
         X_bc, u_bc,
         X_cf, n_cf,
         xub, xlb, uub, ulb,
         modelPath, reloadModel,
-        mu=1.1060e8,
+        mu=1.2680e8,
         loss_weights=[1e-4, 1e-3, 1e-2, 1e-4, 1e-6])
 
 X_u = pinn.tensor(X_u_train)
