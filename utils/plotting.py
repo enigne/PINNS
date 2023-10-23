a78cbaf743ec1635603fdfce76961f7b83812332
Cheng Gong <enigne@gmail.com>
2023-10-23 19:38:41 +0000

    save plot 1D and 2D

--- utils/plotting.py
+++ utils/plotting.py
@@ -719,3 +719,79 @@ def plot_1D_solutions_all(pinn, X_f, X_star, u_star, xlb, xub, vranges={}, saveP
     else:
         plt.show()
     #}}}
+def plot_1D_transient_solutions(pinn, X_f, X_star, u_star, X_1d, C_1d, xlb, xub, vranges={}, savePath=None): #{{{
+    yts = 3600*24*365
+    # predicted C solution, only look at X_1d
+    C_nn = pinn.C_model(X_1d)
+    plotData1D = {}
+    plotData1D['C - C obs'] = abs(C_nn) - abs(C_1d)
+    plotData1D['C and C obs'] = np.hstack([abs(C_nn), abs(C_1d)])
+    fig, axs = plt.subplots(1, 3, figsize=(16,5))
+
+    for ax,name in zip(axs.ravel(), plotData1D.keys()):
+        if 'f1' in name:
+            im = ax.plot(X_1d, plotData1D[name], 'o')
+        else:
+            im = ax.plot(X_1d, plotData1D[name])
+        ax.set_title(name)
+    if savePath:
+        plt.savefig(savePath+"/1Dsolution.png")
+    else:
+        plt.show()
+        
+    # 2D solutions x-t plane
+    X, T = np.meshgrid(np.linspace(xlb[0],xub[0],200), np.linspace(xlb[1],xub[1], 200))
+    # obs
+    u = yts*griddata(X_star, u_star[:,0].flatten(), (X, T), method='cubic')
+    h_obs = griddata(X_star, u_star[:,1].flatten(), (X, T), method='cubic')
+    H_obs = griddata(X_star, u_star[:,2].flatten(), (X, T), method='cubic')
+    smb_obs = yts*griddata(X_star, u_star[:,3].flatten(), (X, T), method='cubic')
+
+    # predicted solution
+    u_pred, h, H, C_pred, smb = pinn.predict(X_star)
+    smb = pinn.smb_model(X_star)
+    u_nn = yts*griddata(X_star, u_pred[:,0].flatten(), (X, T), method='cubic')
+    h_nn = griddata(X_star, h[:,0], (X, T), method='cubic')
+    H_nn = griddata(X_star, H[:,0], (X, T), method='cubic')
+    smb_nn = yts*griddata(X_star, smb[:,0], (X, T), method='cubic')
+
+    # residual
+    fSSA, fH = pinn.f_model()
+    FSSA = griddata(X_f, fSSA[:,0], (X, T), method='cubic')
+    FH = yts*griddata(X_f, fH[:,0], (X, T), method='cubic')
+    
+    ###########################
+    plotData = {}
+    plotData['u obs'] = u
+    plotData['h obs'] = h_obs
+    plotData['H obs'] = H_obs
+    plotData['smb obs'] = smb_obs
+    plotData['fSSA residual'] = FSSA
+    ###########################
+    plotData['u nn'] = u_nn
+    plotData['h nn'] = h_nn
+    plotData['H nn'] = H_nn
+    plotData['smb nn'] = smb_nn
+    plotData['fH residual'] = FH
+    ###########################
+    plotData['u - u obs'] = u_nn - u
+    plotData['h - h obs'] = h_nn - h_obs
+    plotData['H - H obs'] = H_nn - H_obs
+    plotData['smb - smb obs'] = smb_nn - smb_obs
+
+    fig, axs = plt.subplots(3, 5, figsize=(24,12))
+
+    for ax,name in zip(axs.ravel(), plotData.keys()):
+        vr = vranges.setdefault(name, [None, None])
+        im = ax.imshow(plotData[name], interpolation='nearest', cmap='rainbow',
+                extent=[X.min(), X.max(), T.min()/yts, T.max()/yts],
+                vmin=vr[0], vmax=vr[1],
+                origin='lower', aspect='auto')
+        ax.set_title(name)
+        fig.colorbar(im, ax=ax, shrink=1)
+
+    if savePath:
+        plt.savefig(savePath+"/2Dsolution.png")
+    else:
+        plt.show()
+    #}}}
