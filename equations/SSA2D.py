bade99cddec896ea4d8089eb1eefe922aed8baef
Cheng Gong <enigne@gmail.com>
2023-10-30 14:56:58 -0400

    SSA2D friction NN test

--- equations/SSA2D.py
+++ equations/SSA2D.py
@@ -372,14 +372,14 @@ class SSA2D_3NN_solve_vel(SSA2D): #{{{
             loss_weights=[1e-5, 1e-3, 1e-5, 1e-8, 1e-18]):
         super().__init__(hp, logger, X_f, 
                 X_bc, u_bc, X_cf, n_cf,
-                xub, xlb, uub[0:1], ulb[0:1],
+                xub, xlb, uub[0:2], ulb[0:2],
                 modelPath, reloadModel,
                 mu, loss_weights=loss_weights)
         # hp["h_layers"] defines h and H model
-        self.h_model = create_NN(hp["h_layers"], inputRange=(xlb, xub), outputRange=(ulb[1:3], uub[1:3]))
+        self.h_model = create_NN(hp["h_layers"], inputRange=(xlb, xub), outputRange=(ulb[2:4], uub[2:4]))
 
         # hp["C_layers"] defines C model
-        self.C_model = create_NN(hp["C_layers"], inputRange=(xlb, xub), outputRange=(ulb[3:4], uub[3:4]))
+        self.C_model = create_NN(hp["C_layers"], inputRange=(xlb, xub), outputRange=(ulb[4:5], uub[4:5]))
 
         self.trainableLayers = (self.model.layers[1:-1]) + (self.h_model.layers[1:-1]) + (self.C_model.layers[1:-1])
         self.trainableVariables = self.model.trainable_variables + self.h_model.trainable_variables + self.C_model.trainable_variables
@@ -473,5 +473,209 @@ class SSA2D_3NN_solve_vel(SSA2D): #{{{
         output all model summaries
         '''
         return self.model.summary(),self.h_model.summary(), self.C_model.summary()
+
+    #}}}
+class SSA2D_frictionNN(SSA2D): #{{{
+    '''
+    class of learning friction laws from observed u, H, and h, and PDEs
+    '''
+    def __init__(self, hp, logger, X_f, 
+            X_bc, u_bc, X_cf, n_cf, 
+            xub, xlb, uub, ulb, 
+            modelPath, reloadModel,
+            mu, n=3.0, 
+            loss_weights=[1e-5, 1e-3, 1e-5, 1e-8, 1e-12]):
+        super().__init__(hp, logger, X_f, 
+                X_bc, u_bc, X_cf, n_cf,
+                xub, xlb, uub[0:2], ulb[0:2],
+                modelPath, reloadModel,
+                mu, loss_weights=loss_weights)
+        # hp["h_layers"] defines h and H model
+        self.h_model = create_NN(hp["h_layers"], inputRange=(xlb, xub), outputRange=(ulb[2:4], uub[2:4]))
+
+        # hp["C_layers"] defines C model
+        self.C_model = create_NN(hp["C_layers"], inputRange=(xlb, xub), outputRange=(ulb[4:5], uub[4:5]))
+
+        # hp["friction_layers"] defines C model
+        fri_lb = (ulb[4:5]**2)*((ulb[0:1]**2.0+ulb[1:2]**2.0)**(0.5/n))
+        fri_ub = (uub[4:5]**2)*((uub[0:1]**2.0+uub[1:2]**2.0)**(0.5/n))
+        self.friction_model = create_NN(hp["friction_layers"], inputRange=(np.concatenate([ulb[0:1],ulb[1:2],ulb[4:5]]), np.concatenate([uub[0:1],uub[1:2],uub[4:5]])), outputRange=(fri_lb, fri_ub))
+
+        self.trainableLayers = (self.model.layers[1:-1]) + (self.h_model.layers[1:-1]) + (self.C_model.layers[1:-1]) + (self.friction_model.layers[1:-1])
+        self.trainableVariables = self.model.trainable_variables + self.h_model.trainable_variables + self.C_model.trainable_variables + self.friction_model.trainable_variables
+        
+    # need to overwrite nn_model, which is used in computing the loss function
+    @tf.function
+    def nn_model(self, X):
+        '''
+        get the velocity and derivative prediction from the NN
+        '''
+        x = X[:, 0:1]
+        y = X[:, 1:2]
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(x)
+            tape.watch(y)
+            Xtemp = tf.concat([x, y], axis=1)
+
+            uv_sol = self.model(Xtemp)
+            u = uv_sol[:, 0:1]
+            v = uv_sol[:, 1:2]
+
+            sH_sol = self.h_model(Xtemp)
+            s = sH_sol[:, 0:1]
+            H = sH_sol[:, 1:2]
+
+            C = self.C_model(Xtemp)
+
+        u_x = tape.gradient(u, x)
+        v_x = tape.gradient(v, x)
+        u_y = tape.gradient(u, y)
+        v_y = tape.gradient(v, y)
+        del tape
+
+        return u, v, u_x, v_x, u_y, v_y, s, H, C
+
+    @tf.function
+    def f_model(self):
+        '''
+        The actual PINN
+        '''
+        # viscosity
+        mu = self.mu
+        n = self.n
+
+        # Using the new GradientTape paradigm of TF2.0,
+        # which keeps track of operations to get the gradient at runtime
+        with tf.GradientTape(persistent=True) as tape:
+            # Watching the two inputs we’ll need later, x and y
+            tape.watch(self.x_f)
+            tape.watch(self.y_f)
+
+            # just rename the input
+            X_f = tf.concat([self.x_f, self.y_f], axis=1)
+
+            # Getting the prediction
+            u, v, u_x, v_x, u_y, v_y, s, H, C = self.nn_model(X_f)
+
+            eta = 0.5*mu *(u_x**2 + v_y**2 + 0.25*(u_y+v_x)**2 + u_x*v_y+1.0e-30)**(0.5*(1.0-n)/n)
+            # stress tensor
+            etaH = eta * H
+            B11 = etaH*(4*u_x + 2*v_y)
+            B22 = etaH*(4*v_y + 2*u_x)
+            B12 = etaH*(  u_y +   v_x)
+                        
+            # use NN to predict the basal stress
+            tempX = tf.concat([u, v, C], axis=1)
+            taub = self.friction_model(tempX) 
+
+        # Getting the other derivatives
+        sigma11 = tape.gradient(B11, self.x_f)
+        sigma12 = tape.gradient(B12, self.y_f)
+
+        sigma21 = tape.gradient(B12, self.x_f)
+        sigma22 = tape.gradient(B22, self.y_f)
+
+        # surface gradient
+        s_x = tape.gradient(s, self.x_f)
+        s_y = tape.gradient(s, self.y_f)
+
+        # Letting the tape go
+        del tape
+
+        # compute the basal stress
+        u_norm = (u**2+v**2)**0.5
+
+        f1 = sigma11 + sigma12 - taub*u/(u_norm+1e-30) - self.rhoi*self.g*H*s_x
+        f2 = sigma21 + sigma22 - taub*v/(u_norm+1e-30) - self.rhoi*self.g*H*s_y
+
+        return f1, f2
+    
+    @tf.function
+    def loss(self, uv, X_u):
+        '''
+        loss = |u-uobs|+|v-vobs|+|h-hobs|+|H-Hobs|+|f1|+|f2|
+        '''
+        # match h, H, and C to the training data
+        u0 = uv[:,0:1]
+        v0 = uv[:,1:2]
+        s0 = uv[:,2:3]
+        H0 = uv[:,3:4]
+        C0 = uv[:,4:5]
+
+        uv_pred = self.model(X_u)
+        u0_pred = uv_pred[:,0:1]
+        v0_pred = uv_pred[:,1:2]
+
+        sH_pred = self.h_model(X_u)
+        s0_pred = sH_pred[:,0:1]
+        H0_pred = sH_pred[:,1:2]
+
+        C0_pred = self.C_model(X_u)
+
+        # f_model on the collocation points 
+        f1_pred, f2_pred = self.f_model()
+        # calving front
+        fc1_pred,fc2_pred = self.cf_model(self.X_cf, self.n_cf)
+
+        # velocity misfit
+        # velocity misfit
+        mse_u = self.loss_weights[0]*(self.yts**2) * tf.reduce_mean(tf.square(u0 - u0_pred))
+        mse_v = self.loss_weights[0]*(self.yts**2) * tf.reduce_mean(tf.square(v0 - v0_pred))
+        # geometry misfit
+        mse_s = self.loss_weights[1]*tf.reduce_mean(tf.square(s0 - s0_pred))
+        mse_H = self.loss_weights[1]*tf.reduce_mean(tf.square(H0 - H0_pred))
+        # friction misfit
+        mse_C = self.loss_weights[2]*tf.reduce_mean(tf.square(C0 - C0_pred))
+        # residual of PDE
+        mse_f1 = self.loss_weights[3]*tf.reduce_mean(tf.square(f1_pred))
+        mse_f2 = self.loss_weights[3]*tf.reduce_mean(tf.square(f2_pred))
+        # calving front boundary
+        mse_fc1 = self.loss_weights[4]*tf.reduce_mean(tf.square(fc1_pred))
+        mse_fc2 = self.loss_weights[4]*tf.reduce_mean(tf.square(fc2_pred))
+
+        # sum the total
+        totalloss = mse_u + mse_v + mse_s + mse_H + mse_C + mse_f1 + mse_f2 + mse_fc1 + mse_fc2
+        return {"loss": totalloss, "mse_u": mse_u, "mse_v": mse_v, "mse_s": mse_s,
+                "mse_H": mse_H, "mse_C": mse_C, "mse_f1": mse_f1, "mse_f2": mse_f2,
+                "mse_fc1": mse_fc1, "mse_fc2": mse_fc2}
+
+    @tf.function
+    def test_error(self, X_star, u_star):
+        '''
+        test error of taub
+        '''
+        uv_pred = self.model(X_star)
+        u_pred = uv_pred[:,0:1]
+        v_pred = uv_pred[:,1:2]
+        C_pred = self.C_model(X_star)
+        
+        uC_pred = tf.concat([u_pred, v_pred, C_pred], axis=1)
+        sol_pred = self.friction_model(uC_pred)
+        
+        # ref taub
+        ref_sol = u_star[:,4:5]**2*((u_star[:,0:1]**2.0+u_star[:,1:2]**2.0)**(1.0/6.0))
+        
+        return  tf.math.reduce_euclidean_norm(tf.math.abs(sol_pred) - tf.math.abs(ref_sol)) / tf.math.reduce_euclidean_norm(ref_sol)
+    
+    def predict(self, X_star):
+        '''
+        return numpy array of the model
+        '''
+        uv_pred = self.model(X_star)
+        u_pred = uv_pred[:, 0:1]
+        v_pred = uv_pred[:, 1:2]
+
+        hH_pred = self.h_model(X_star)
+        h_pred = hH_pred[:, 0:1]
+        H_pred = hH_pred[:, 1:2]
+        C_pred = self.C_model(X_star)
+
+        return u_pred.numpy(), v_pred.numpy(), h_pred.numpy(), H_pred.numpy(), C_pred.numpy()
+
+    def summary(self):
+        '''
+        output all model summaries
+        '''
+        return self.model.summary(),self.h_model.summary(), self.C_model.summary(), self.friction_model.summary()
 
     #}}}
