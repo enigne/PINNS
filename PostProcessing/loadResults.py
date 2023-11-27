bc62236c1d6f01f425efb43e75c27d0c4d1aedb7
Cheng Gong <enigne@gmail.com>
2023-11-27 11:35:30 -0500

    postproc data

--- PostProcessing/loadResults.py
+++ PostProcessing/loadResults.py
@@ -8,7 +8,6 @@ from math import sqrt
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Util functions {{{
 def getListOfVaribles(x, keyword='weights', length=5): #{{{
     # find the index start with keyword
     index = [i for i, s in enumerate(x) if keyword in s]
@@ -38,24 +37,12 @@ def getFromRegex(x, expression): #{{{
                 return int(nn.group(0))
             except:
                 return nn.group(0) #}}}
-def load_history(projPath, filename="history.json"): #{{{
-    '''
-    load history data
-    '''
-    modelPath = projPath + "/" + filename
-    with open(modelPath,  'rb') as f:
-        data = pickle.load(f)
-    return data#}}}
-def get_final_errors(data): #{{{
-    keys = data.keys()
-    return {k:data[k][-1] for k in keys}
-    #}}}
 def upscale_by_weights(dataDict, keys, weights): #{{{
     for k,w in zip(keys,weights):
         dataDict[k] = (dataDict[k]/w) if k in dataDict else dataDict[k]
     return dataDict
     #}}}
-def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN='3NN', neurons=20, layers=8, projPath="./Models_Kubeflow/Models/", C_true=None): #{{{
+def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN='3NN', neurons=20, layers=4, projPath="./Models_Kubeflow/Models/", C_true=None): #{{{
     # weights
     wu = 5
     wC = 5
@@ -85,6 +72,8 @@ def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN=
         # load all history data
         dataList = [load_history(projPath+name) for name in df[(df['wu']==weights[0]) &
                                                  (df['wf']==weights[3]) &
+                                                 (df['wh']==weights[1]) &
+                                                 (df['wfc-wf']==dwfc) &
                                                  (df['prefix']==prefix) &
                                                  (df['NN']==NN) &
                                                  (df['neurons']==neurons) &
@@ -104,7 +93,7 @@ def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN=
 
     # colors indicates different weights combinations
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-    #    lims = [[1e3, 1e8], [1e0, 1e6], [1e0, 1e6], [1e-1, 1e13], [1,1e18], [1e4,1e6]]
+    lims = [[1e3, 1e8], [1e0, 1e6], [1e0, 1e6], [1e-1, 1e13], [1e4,1e7]]
 
     features = keys
     Nkey = len(keys)-1
@@ -119,11 +108,11 @@ def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN=
         for i in range(Nkey):
             for j in range(i,Nkey):
                 ax = axs[i][j]
-                ax.scatter(err[features[j+1]], err[features[i]], c=colors[cid], label=label)
+                ax.scatter(err[features[j+1]], err[features[i]], c=colors[cid], label=label, s=20)
                 ax.set_xscale('log')
                 ax.set_yscale('log')
-#                 ax.set_xlim(lims[j+1])
-#                 ax.set_ylim(lims[i])
+                ax.set_xlim(lims[j+1])
+                ax.set_ylim(lims[i])
 
         # add labels
         for i in range(len(features)-1):
@@ -136,11 +125,33 @@ def getDataDict(df, wh=5, lossWeightDict={}, dwfc=10, wf=[], prefix='SSA1D', NN=
     ax.legend(bbox_to_anchor=(1.1, 1.5))
 
     return dataDict #}}}
+def loadHistory(folder, projPath="./Models_Kubeflow/Models/", filename="history.json", N=None): #{{{
+    '''
+    load history data to a dictionary
+    '''
+    modelPath = projPath + folder + "/" + filename
+    with open(modelPath,  'rb') as f:
+        data = pickle.load(f)
+    errorLength = getErrorLength(data)
+    if N:
+        return getLastNErrors(data,N), errorLength
+    return data, errorLength#}}}
+def getLastNErrors(data, N=10): #{{{
+    keys = data.keys()
+    return {k:data[k][-N-1:-1] for k in keys}
+    #}}}
+def getFinalErrors(data): #{{{
+    return getLastNErrors(data, N=1)
+    #}}}
+def getErrorLength(data): #{{{
+    keys = list(data.keys())
+    return len(data[keys[0]])
+    #}}}
 def findAllExps(projPath="./Models_Kubeflow/Models/", dimensions = 1): #{{{
     # find all the experiments folders
     foldersList = os.listdir(projPath)
     df = pd.DataFrame(foldersList, columns=['Name'])
-    
+
     # get info from the name
     df['sp'] = df['Name'].apply(lambda x: x.split('_'))
     # remove len(['sp'])>3
@@ -163,7 +174,7 @@ def findAllExps(projPath="./Models_Kubeflow/Models/", dimensions = 1): #{{{
     df['noise'] = df['sp'].apply(lambda x: getListOfVaribles(x,'noise',5))
     df = df.drop(columns=['sp'])
     df['wfc-wf'] = df['wfc']-df['wf']
-    
+
     # remove name with 'seed'
     print('-> size before cleanup:', df.shape)
     df = df[df['Date'].str.strip()!='seed']
@@ -171,7 +182,8 @@ def findAllExps(projPath="./Models_Kubeflow/Models/", dimensions = 1): #{{{
 
     # convert date
     df['Date']=df['Date'].astype(int)
-    
+    df['Time']=df['Time'].astype(str)
+
     # different experiment types
     print('-> Keys before cleanup:', df['prefix'].unique())
     # remove unused data
@@ -190,7 +202,7 @@ def findAllExps(projPath="./Models_Kubeflow/Models/", dimensions = 1): #{{{
     else:
         lossWeightDict = {'mse_u':0, 'mse_H': 1, 'mse_h':1, 'mse_f1': 3, 'test':-1}
         prefix = 'SSA1D'
-        
+
         # only look at SSA1D
         print('-> Keys before cleanup:', df['prefix'].unique())
         # remove unused data
@@ -199,11 +211,41 @@ def findAllExps(projPath="./Models_Kubeflow/Models/", dimensions = 1): #{{{
 
     # add columns for loading errors
     numCol = df.shape[1]
-    df.insert(numCol, 'history length', 0) 
+    df.insert(numCol, 'history length', 0)
     for k in lossWeightDict.keys():
-        df.insert(numCol, k, 0)   
-        df.insert(numCol, k+"mean10", 0)
-        df.insert(numCol, k+"10", 0)
-    
-    return df #}}}
+        df.insert(numCol, k, 0.0)
+        df.insert(numCol, k+"mean100", 0.0)
+        df.insert(numCol, k+"100", 0.0)
+        df[ k+"100"] = df[ k+"100"].astype(object)
 
+    df.reset_index(inplace=True)
+    return df #}}}
+def addErrors(df, N=100): #{{{
+    '''
+    load history.json for each rows in the df and add errors to the corresponding columns
+    '''
+    # loop over the dataframe
+    for i in df.index:
+        if i % 100 == 0:
+            print(f"Loop over the whole dataframe: {i}/{df.shape[0]}")
+        name = df.iloc[i]['Name']
+        data, errL = loadHistory(name, N=N)
+        df.at[i, 'history length'] = errL
+        # save the last N data to df
+        for k in data.keys():
+            # mse in the last N errors
+            newk = k + str(N)
+            if newk in df:
+                df.at[i, newk] = data[k][-N-1:-1]
+
+    # get the last one and mean
+    name = df.iloc[0]['Name']
+    data, errL = loadHistory(name, N=N)
+    for k in data.keys():
+        refk = k + str(N)
+        newk = k + "mean" + str(N)
+        if (k in df) and (refk in df) and (newk in df):
+            df[k] = df[refk].apply(lambda x: x[-1] if isinstance(x, list) else 0)
+            df[newk] = df[refk].apply(lambda x: np.mean(x))
+
+    return df #}}}
