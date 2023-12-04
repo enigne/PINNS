bfa8da91ff13c8b81d4ab9d5b3ac3181305003bb
Cheng Gong <enigne@gmail.com>
2023-12-04 09:45:04 -0500

    rewrite 1D loaddata

--- utils/loaddata.py
+++ utils/loaddata.py
@@ -734,3 +734,106 @@ def prep_2D_data_withmu(path, N_f=None, N_u=None, N_s=None, N_H=None, N_C=None, 
     n_cf = np.hstack((nx.flatten()[:,None], ny.flatten()[:,None]))
 
     return x, y, Exact_vx, Exact_vy, X_star, u_star, X_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb, None  #}}}
+def prep_1D_data_all(path, N_f=None, N_u=None, N_s=None, N_H=None, N_C=None, FlightTrack=False): #{{{
+    # Reading 1D SSA ref solutions: x-coordinate, provide ALL the variables in u_train
+    data = scipy.io.loadmat(path,  mat_dtype=True)
+
+    # viscosity
+    mu = data['mu']
+
+    # Flatten makes [[]] into [], [:,None] makes it a column vector
+    x = data['x'].flatten()[:,None]
+
+    # collocation points
+    X_f = np.real(data['X_f'])
+    idf = np.random.choice(X_f.shape[0], N_f, replace=False)
+    X_f = X_f[idf,:]
+
+    # real() is to make it float by default, in case of zeroes
+    Exact_vel = np.real(data['vel'].flatten()[:,None])
+    Exact_h = np.real(data['h'].flatten()[:,None])
+    Exact_H = np.real(data['H'].flatten()[:,None])
+    Exact_C = np.real(data['C'].flatten()[:,None])
+
+    # boundary nodes
+    DBC = data['DBC'].flatten()[:,None]
+
+    # Preparing the inputs x and y for predictions in one single array, as X_star
+    X_star = x
+
+    # Preparing the testing u_star and vy_star
+    u_star = np.hstack((Exact_vel.flatten()[:,None], Exact_h.flatten()[:,None], Exact_H.flatten()[:,None], Exact_C.flatten()[:,None] )) 
+
+    # Domain bounds: for regularization and generate training set
+    xlb = X_star.min(axis=0)
+    xub = X_star.max(axis=0) 
+    umin = u_star.min(axis=0)
+    umax = u_star.max(axis=0) 
+    ulb = {}
+    uub = {}
+    ulb["uv"] = umin[0:1]
+    ulb["sH"] = umin[1:3]
+    uub["sH"] = umax[1:3]
+    ulb["C"] = umin[3:4]
+    uub["C"] = umax[3:4]
+
+    # set Dirichlet boundary conditions
+    idbc = np.transpose(np.asarray(DBC>0).nonzero())
+    X_bc = X_star[idbc[:,0],:]
+    u_bc = u_star[idbc[:,0],:]
+
+    # Stacking them in multidimensional tensors for training, only use ice covered area
+    icemask = data['icemask'].flatten()[:,None]
+    iice = np.transpose(np.asarray(icemask>0).nonzero())
+    X_ = np.vstack([X_star[iice[:,0],:]])
+    u_ = np.vstack([u_star[iice[:,0],:]])
+
+    # Getting the corresponding X_train and u_train(which is now scarce boundary/initial coordinates)
+    X_train = {}
+    u_train = {}
+
+    # Generating a uniform random sample from ints between 0, and the size of x_u_train, of size N_u (initial data size) and without replacement (unique)
+    # velocity data
+    if N_u:
+        idx = np.random.choice(X_.shape[0], N_u, replace=False)
+        X_train["uv"] = X_[idx,:]
+        u_train["uv"] = u_[idx, 0:1]
+    else:
+        X_train["uv"] = X_bc
+        u_train["uv"] = u_bc[:, 0:1]
+
+    # surface elevation, always available, use the maximum points among all the other data set
+    if N_s is None:
+        Nlist = [N_u, N_H, N_C]
+        N_s = max([i for i in Nlist if i is not None])
+
+    idx = np.random.choice(X_.shape[0], N_s, replace=False)
+    X_train["s"] = X_[idx,:]
+    u_train["s"] = u_[idx, 1:2]
+
+    # ice thickness, or bed elevation
+    if N_H:
+        idx = np.random.choice(X_.shape[0], N_H, replace=False)
+        X_train["H"] = X_[idx,:]
+        u_train["H"] = u_[idx, 2:3]
+    else:
+        X_train["H"] = X_bc
+        u_train["H"] = u_bc[:, 2:3]
+
+    # friction coefficients
+    if N_C:
+        idx = np.random.choice(X_.shape[0], N_C, replace=False)
+        X_train["C"] = X_[idx,:]
+        u_train["C"] = u_[idx, 3:4]
+    else:
+        X_train["C"] = X_bc
+        u_train["C"] = u_bc[:, 3:4]
+
+    # calving front info
+    cx = data['cx'].flatten()[:,None]
+    nx = data['nx'].flatten()[:,None]
+
+    X_cf = cx
+    n_cf = nx
+
+    return x, Exact_vel, X_star, u_star, X_train, u_train, X_f, X_bc, u_bc, X_cf, n_cf, xub, xlb, uub, ulb, mu  #}}}
